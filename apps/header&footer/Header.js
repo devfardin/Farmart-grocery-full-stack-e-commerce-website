a0715ccfbdbbdd9760bea8b22ba2e8a9ad1ac3f1
fardin-ahmed-1 <contactfardin22@gmail.com>
2024-02-19 12:24:17 +0600

    upload all files

---
+++ apps/header&footer/Header.js
@@ -0,0 +1,13 @@
+import Container from "../components/shared/Container"
+
+const Header = () => {
+  return (
+    <Container>
+        
+        <h1 className="text-xl">Header </h1>
+      
+     </Container> 
+  )
+}
+
+export default Header
